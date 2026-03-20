50102ccc1ccabf0e9b00dc138997811c68008a8c
RubenMeju <rubenmeju@outlook.es>
2026-03-21 00:40:24 +0100

    fix serializer

--- backend/veladazone/apps/predictions/serializers.py
+++ backend/veladazone/apps/predictions/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Argument, ArgumentReply, Prediction
+from .models import Argument, ArgumentReply, ArgumentVote, Prediction
 from veladazone.apps.fighters.serializers import FightSerializer, FighterSerializer
 
 
@@ -43,7 +43,7 @@ class ArgumentSerializer(serializers.ModelSerializer):
     avatar = serializers.SerializerMethodField()
     fighter_name = serializers.SerializerMethodField()
     fighter_flag = serializers.SerializerMethodField()
-    vote_count = serializers.IntegerField(read_only=True)
+    vote_count = serializers.SerializerMethodField()
     replies = ArgumentReplySerializer(many=True, read_only=True)
     user_voted = serializers.SerializerMethodField()
     user_replied = serializers.SerializerMethodField()
@@ -76,44 +76,17 @@ class ArgumentSerializer(serializers.ModelSerializer):
     def get_fighter_flag(self, obj):
         return obj.fighter_supported.country_flag
 
+    def get_vote_count(self, obj):
+        return obj.argument_votes.count()  # type: ignore
+
     def get_user_voted(self, obj):
         request = self.context.get("request")
         if not request or not request.user.is_authenticated:
             return False
-        return obj.argument_votes.filter(user=request.user).exists()
+        return obj.argument_votes.filter(user=request.user).exists()  # type: ignore
 
     def get_user_replied(self, obj):
         request = self.context.get("request")
         if not request or not request.user.is_authenticated:
             return False
-        return obj.replies.filter(user=request.user).exists()
-
-    username = serializers.SerializerMethodField()
-    avatar = serializers.SerializerMethodField()
-    fighter_name = serializers.SerializerMethodField()
-    fighter_flag = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Argument
-        fields = [
-            "id",
-            "username",
-            "avatar",
-            "fighter_name",
-            "fighter_flag",
-            "text",
-            "votes",
-            "created_at",
-        ]
-
-    def get_username(self, obj):
-        return obj.user.display_name  # type: ignore
-
-    def get_avatar(self, obj):
-        return obj.user.avatar_url  # type: ignore
-
-    def get_fighter_name(self, obj):
-        return obj.fighter_supported.name
-
-    def get_fighter_flag(self, obj):
-        return obj.fighter_supported.country_flag
+        return obj.replies.filter(user=request.user).exists()  # type: ignore
