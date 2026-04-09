5be901317f248182272a3dbc04178828592aa1f6
RubenMeju <rubenmeju@outlook.es>
2026-04-09 21:45:16 +0200

    añadir username al serializer

--- backend/veladazone/apps/users/serializers.py
+++ backend/veladazone/apps/users/serializers.py
@@ -5,5 +5,5 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'twitch_username', 'avatar_url', 'created_at']
+        fields = ["id", "username", "twitch_username", "avatar_url", "created_at"]
         read_only_fields = fields
